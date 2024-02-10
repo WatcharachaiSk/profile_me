0aec8fc2dd1dfd4494bb4d9470927cea324f1be3
Watcharachai Samkham <watcharachai.sa@rmuti.ac.th>
2024-02-10 17:52:41 +0700

    fix home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // import { useState } from "react";
 import './App.css';
-import RoutesPath from './router/RoutesPath';
+import HomePage from './pages/home/HomePage';
+// import RoutesPath from './router/RoutesPath';
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className="h-screen w-screen">
       <div className="flex ">
-        <RoutesPath />
+        <HomePage />
       </div>
     </div>
   );
